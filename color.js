fc78463fa03e8572b1795b75513772739c699097
Yanran Lu <luyanran@yahoo.com>
2015-01-30 15:00:29 -0800

    add Hide

--- color.js
+++ color.js
@@ -4,6 +4,7 @@
 	var heightDiv = Math.floor(height / 255);
 	var widthDiv =  Math.floor(width / 255);
 	var sumDiv = Math.floor((height + width) / 255)
+	
 	$("body").mousemove(function(event) {
 		// debugger
 		console.log(event.clientX + " " + sumDiv + " " + (heightDiv + widthDiv))
